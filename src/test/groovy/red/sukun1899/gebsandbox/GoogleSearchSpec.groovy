512bade3d1080faf742686198eaf07501dc8b298
su-kun1899 <higedrum.coz@gmail.com>
2017-11-03 14:52:39 +0900

    add skelton test

---
+++ src/test/groovy/red/sukun1899/gebsandbox/GoogleSearchSpec.groovy
@@ -0,0 +1,13 @@
+package red.sukun1899.gebsandbox
+
+import geb.spock.GebSpec
+
+/**
+ * @author su-kun1899
+ */
+class GoogleSearchSpec extends GebSpec {
+    def "Googleのトップページを表示する"() {
+        expect:
+        1 == 1
+    }
+}
---
+++ src/test/resources/GebConfig.groovy
@@ -0,0 +1,39 @@
+/*
+	This is the Geb configuration file.
+
+	See: http://www.gebish.org/manual/current/configuration.html
+*/
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeDriverService
+import org.openqa.selenium.os.ExecutableFinder
+
+import static org.apache.commons.lang3.SystemUtils.IS_OS_LINUX
+import static org.apache.commons.lang3.SystemUtils.IS_OS_MAC
+
+static File findDriverExecutable() {
+    def defaultExecutable = new ExecutableFinder().find("chromedriver")
+    if (defaultExecutable) {
+        new File(defaultExecutable)
+    } else {
+        new File("drivers").listFiles().findAll {
+            !it.name.endsWith(".version")
+        }.find {
+            if (IS_OS_LINUX) {
+                it.name.contains("linux")
+            } else if (IS_OS_MAC) {
+                it.name.contains("mac")
+            }
+        }
+    }
+}
+
+//noinspection GroovyUnusedAssignment
+driver = {
+    ChromeDriverService.Builder serviceBuilder = new ChromeDriverService.Builder()
+            .usingAnyFreePort()
+            .usingDriverExecutable(findDriverExecutable())
+    new ChromeDriver(serviceBuilder.build())
+}
+
+//noinspection GroovyUnusedAssignment
+baseUrl = "http://gebish.org"
