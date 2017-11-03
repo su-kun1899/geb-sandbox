ebe6dd026aa8f4ac14b70beb7ede19e3a5fdf623
su-kun1899 <higedrum.coz@gmail.com>
2017-11-03 16:36:15 +0900

    refactor url

--- src/test/groovy/red/sukun1899/gebsandbox/GoogleTopPage.groovy
+++ src/test/groovy/red/sukun1899/gebsandbox/GoogleTopPage.groovy
@@ -6,6 +6,6 @@ import geb.Page
  * @author su-kun1899
  */
 class GoogleTopPage extends Page {
-    static url = "http://www.google.com"
+    static url = "https://www.google.co.jp/"
     static at = { title == "Google"}
 }
